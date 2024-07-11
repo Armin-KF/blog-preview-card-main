7b7193c835756a9e5712795f4ded7c6b3bb20717
ArminKafe <arminkaramifard2@gmail.com>
2024-07-11 12:21:18 +0330

    chore: Update card dimensions and styles

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,18 @@ function App() {
           />
           <div>
             <h6 id="my-card-h6">Learning</h6>
+            <p id="my-card-p">Published 21 Dec 2023</p>
+            <h1 id="my-card-h1">HTML & CSS foundations</h1>
+            <p id="my-card-text">
+              These languages are the backbone of every website , defining
+              structure, content, and presentation
+            </p>
+            <img
+              id="profile-pic"
+              src="assets/images/image-avatar.webp"
+              alt="profile-pic"
+            />
+            <p id="profile-name">Greg Hooper</p>
           </div>
         </div>
       </header>
