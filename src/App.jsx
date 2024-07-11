88f50b561c667502b4e3ab785fe2ecdde9e876c7
ArminKafe <arminkaramifard2@gmail.com>
2024-07-11 09:48:24 +0330

    chore: Add .gitignore file and update style guide and index.html

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div id="my-card">
+          <img
+            id="my-card-img"
+            src="assets/images/illustration-article.svg"
+            alt="image"
+          />
+          <div>
+            <h6 id="my-card-h6">Learning</h6>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
